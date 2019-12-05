ec4040f8f7f318aecfc78706257c34c655fa7a00
Vitaly Oryschuk <Leilyua@ukr.net>
2019-12-05 18:02:12 +0200

    Updates

---
+++ precache-manifest.51a51120fdf87e498858131d0ec54eae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cfc8258e91e9ba420cc834924d1211",
+    "url": "/VitalyOryschuk/goit-react-hw-01-components.git/index.html"
+  },
+  {
+    "revision": "434e584589f6b870d566",
+    "url": "/VitalyOryschuk/goit-react-hw-01-components.git/static/js/2.2f0d4e63.chunk.js"
+  },
+  {
+    "revision": "592be86b6e6b627a7ad4",
+    "url": "/VitalyOryschuk/goit-react-hw-01-components.git/static/js/main.d072f2e5.chunk.js"
+  },
+  {
+    "revision": "5a4a278d5cc1d9b7a030",
+    "url": "/VitalyOryschuk/goit-react-hw-01-components.git/static/js/runtime-main.71126cbd.js"
+  }
+]);
