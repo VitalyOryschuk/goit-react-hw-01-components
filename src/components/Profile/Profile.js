b0fcea868bc30e70423aeccc7416ac064b021270
Vitaly Oryschuk <Leilyua@ukr.net>
2019-12-05 15:24:46 +0200

    added Profile

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import T from 'prop-types';
+// import user from './components/user.json';
+
+const Profile = ({ user }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={user.avatar} alt="user avatar" className="avatar" />
+        <p className="name">{user.name}</p>
+        <p className="tag">{user.tag}</p>
+        <p className="location">{user.location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{user.stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{user.stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{user.stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.defaultProps = {
+  user: {},
+};
+
+Profile.propTypes = {
+  user: T.shape({
+    name: T.string.isRequired,
+    tag: T.string.isRequired,
+    location: T.string.isRequired,
+    avatar: T.string.isRequired,
+    stats: T.shape({
+      followers: T.number.isRequired,
+      views: T.number.isRequired,
+      likes: T.number.isRequired,
+    }),
+  }),
+};
+
+export default Profile;
