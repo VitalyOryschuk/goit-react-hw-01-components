8edf186c65be720716ac8723db11c24d655f74f8
Vitaly Oryschuk <Leilyua@ukr.net>
2019-12-08 21:00:02 +0200

    added styles-css

---
+++ build/precache-manifest.6e3c6c74beb3004cd8f85f0f00457b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784aacd4d49c668fa3360216da1421bb",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b9361b5b5d699a4050f0",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6abc32f20ad0574dc480",
+    "url": "/goit-react-hw-01-components/static/css/main.2d34b771.chunk.css"
+  },
+  {
+    "revision": "b9361b5b5d699a4050f0",
+    "url": "/goit-react-hw-01-components/static/js/2.65bd1d9c.chunk.js"
+  },
+  {
+    "revision": "6abc32f20ad0574dc480",
+    "url": "/goit-react-hw-01-components/static/js/main.6a1d50cd.chunk.js"
+  },
+  {
+    "revision": "c4036953cb714bd39313",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.02e29828.js"
+  }
+]);
